319f56a283867ba9a6ada04788ff870045be91af
Jeff Spradlin <jeff.spradlin@slalom.com>
2020-11-02 14:31:06 -0600

    removed loadbalancer

---
+++ modules/consul-cluster/consul-cluster.tf
@@ -0,0 +1,106 @@
+#---------------------------------------------------------------------------------------------------------------------
+# CREATE A VIRTUAL MACHINE SCALE SET TO RUN CONSUL
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_virtual_machine_scale_set" "consul" {
+  name = var.cluster_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name = var.instance_size
+    tier = var.instance_tier
+    capacity = var.cluster_size
+  }
+
+  os_profile {
+    computer_name_prefix = var.computer_name_prefix
+    admin_username = var.admin_user_name
+
+    #This password is unimportant as it is disabled below in the os_profile_linux_config
+    admin_password = var.default_password
+    custom_data = var.custom_data
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path = "/home/${var.admin_user_name}/.ssh/authorized_keys"
+      key_data = file(var.key_data)
+    }
+  }
+
+  network_profile {
+    name = "ConsulNetworkProfile"
+    primary = true
+
+    ip_configuration {
+      name = "ConsulIPConfiguration"
+      primary = true
+      subnet_id = var.subnet_id
+    }
+  }
+
+  storage_profile_image_reference {
+    id = var.image_id
+  }
+
+  storage_profile_os_disk {
+    name = ""
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    os_type = "Linux"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  tags = {
+    scaleSetName = var.cluster_name
+  }
+}
+
+#---------------------------------------------------------------------------------------------------------------------
+# CREATE A SECURITY GROUP AND RULES FOR SSH
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_network_security_group" "consul" {
+  name = var.cluster_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_network_security_rule" "ssh" {
+  count = length(var.allowed_ssh_cidr_blocks)
+
+  access = "Allow"
+  destination_address_prefix = "*"
+  destination_port_range = "22"
+  direction = "Inbound"
+  name = "SSH${count.index}"
+  network_security_group_name = azurerm_network_security_group.consul.name
+  priority = 100 + count.index
+  protocol = "Tcp"
+  resource_group_name = var.resource_group_name
+  source_address_prefix = element(var.allowed_ssh_cidr_blocks, count.index)
+  source_port_range = "1024-65535"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# THE CONSUL-SPECIFIC INBOUND/OUTBOUND RULES COME FROM THE CONSUL-SECURITY-GROUP-RULES MODULE
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "security_group_rules" {
+  source = "../consul-security-group-rules"
+
+  security_group_name = azurerm_network_security_group.consul.name
+  resource_group_name = var.resource_group_name
+  allowed_inbound_cidr_blocks = [var.allowed_inbound_cidr_blocks]
+
+  server_rpc_port = var.server_rpc_port
+  cli_rpc_port    = var.cli_rpc_port
+  serf_lan_port   = var.serf_lan_port
+  serf_wan_port   = var.serf_wan_port
+  http_api_port   = var.http_api_port
+  dns_port        = var.dns_port
+}
